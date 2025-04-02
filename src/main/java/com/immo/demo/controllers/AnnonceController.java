245e8dbc8b8113b29b401ef05018fc1679b45b3f
Jérémie P <jpourageaux@hotmail.fr>
2025-04-02 17:21:24 +0200

    add pagination

--- src/main/java/com/immo/demo/controllers/AnnonceController.java
+++ src/main/java/com/immo/demo/controllers/AnnonceController.java
@@ -2,7 +2,13 @@ package com.immo.demo.controllers;
 
 import com.immo.demo.entities.AnnonceEntity;
 import com.immo.demo.services.AnnonceService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +23,18 @@ public class AnnonceController {
     }
 
     @GetMapping("/annonces/liste")
-    public List<AnnonceEntity> list() {
-        List<AnnonceEntity> annonces = this.annonceService.getAllAnnonces();
+    public Page<AnnonceEntity> list(@RequestParam(defaultValue = "0") int page,
+                                    @RequestParam(defaultValue = "2") int size,
+                                    @RequestParam(defaultValue = "id") String sortBy) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+//        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, sortBy));
+
+
+        Page<AnnonceEntity> annonces = this.annonceService.getAllAnnonces(pageable);
+
         return annonces;
     }
+
+
 }
