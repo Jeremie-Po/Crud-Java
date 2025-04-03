d08067c14de34fa92dbe39b16d154d204a508d97
Jérémie P <jpourageaux@hotmail.fr>
2025-04-03 16:50:12 +0200

    fix typo

--- src/main/java/com/immo/demo/controllers/AgenceController.java
+++ src/main/java/com/immo/demo/controllers/AgenceController.java
@@ -1,7 +1,6 @@
 package com.immo.demo.controllers;
 
 import com.immo.demo.entities.AgenceEntity;
-import com.immo.demo.entities.AnnonceEntity;
 import com.immo.demo.services.AgenceService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,6 +15,7 @@ public class AgenceController {
     private final AgenceService agenceService;
 
     public AgenceController(AgenceService agenceService) {
+
         this.agenceService = agenceService;
     }
 
@@ -32,7 +32,7 @@ public class AgenceController {
     }
 
     @GetMapping("/agences/find/{id}")
-    public AgenceEntity find(@PathVariable Long id) {
+    public AgenceEntity find(@PathVariable(name = "id") Long id) {
         AgenceEntity agence = this.agenceService.findOneById(id);
         return agence;
     }
